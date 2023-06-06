b61c9d9f3541dc799912326033673f0f84556d09
celumusam <celmusacydarmlotsa@gmail.com>
2023-06-06 05:08:35 -0700

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,19 +7,19 @@
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
-
-listint_t *slow = list;
-listint_t *fast = list;
-
-if (!list)
-	return (0);
-while (slow && fast && fast->next)
 {
-slow = slow->next;
-fast = fast->next->next;
-if (slow == fast)
-	return (1);
-}
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-return (0);
+	if (!list)
+		return (0);
+	while (slow && fast && fast->next)
+	{	
+		slow,  slow->next;
+		fast, fast->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
 }
