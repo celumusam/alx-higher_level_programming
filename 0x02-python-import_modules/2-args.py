b12a799ace308b27a7629ae6c9e39592c1894075
celumusam <celmusacydarmlotsa@gmail.com>
2023-06-08 03:06:57 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
 
     if size > 1:
         print("{} arguments:".format(size))
-for i in range(1, size + 1):
-    print("{}: {}".format(i, arg[i])
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, arg[i])
 
     elif size == 0:
         print("{} arguments:".format(size))
 
-        elif:
-            print("{} argument:".format(size))
-            print("{}: {}".format(size, arg[i]))
+    elif:
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, arg[i]))
