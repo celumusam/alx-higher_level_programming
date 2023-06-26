1b78746f314024599412aadab2cefb0945d6f650
celumusam <celmusacydarmlotsa@gmail.com>
2023-06-26 14:50:08 +0000

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -14,6 +14,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except Exception as i:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)i
