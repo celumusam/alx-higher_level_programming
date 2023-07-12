f20d76b65ac9ccc704d44856f36003aee0ea184c
celumusam <celmusacydarmlotsa@gmail.com>
2023-07-12 14:34:08 +0000

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+def read_file(filename=""):
+"""Print the contents of a UTF8 text file to stdout."""
+with open(filename, encoding=ütf-8") as f:
+print(f.read(), end="")
