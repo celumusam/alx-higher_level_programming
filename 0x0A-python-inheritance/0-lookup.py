bb0a8889165f12f6b41dcfc87f3899dbefd5bfd7
celumusam <celmusacydarmlotsa@gmail.com>
2023-07-12 12:02:27 +0000

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define an object attribute lookup function."""'
+"""Defines an object attribute lookup function."""
 
 def lookup(obj):
 """Return a list of an object's available atrrributes.""""
